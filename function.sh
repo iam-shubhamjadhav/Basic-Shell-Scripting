408667c010cc9c3bca473c6763601c0a0dce9370
Shubham  Jadhav <111505205+iam-shubhamjadhav@users.noreply.github.com>
2024-05-29 12:25:48 +0530

    Create function.sh

---
+++ function.sh
@@ -0,0 +1,10 @@
+  1
+  2 greet(){
+  3         echo "Hello , $1!"
+  4         #here $1 is used for passing the argument
+  5 }
+  6 greet "shubham"
+  7
+  8 #to display line numbers in vim code editor write this commands
+  9 # press escape :
+ 10 # set nu
